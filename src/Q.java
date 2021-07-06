fc2165c3c94fe2ecc8eb5bbfc0dfb176385ed0c3
Kate <Livia2008@yandex.ru>
2021-07-06 16:19:53 +0300

    added file1

--- src/Q.java
+++ src/Q.java
@@ -7,9 +7,9 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-        LocalDate birthday = LocalDate.of(2021, 11, 30);
-        LocalDate nextBirthday = birthday.plusYears(1);
-        LocalDate firstBirthday = birthday.minusYears(35);
-        System.out.println(nextBirthday);
-        System.out.println(firstBirthday);
+        LocalDate birthday = LocalDate.of( 2021, 11, 30 );
+        LocalDate nextBirthday = birthday.plusYears( 1 );
+        LocalDate firstBirthday = birthday.minusYears( 35 );
+        System.out.println( nextBirthday );
     }
+    
