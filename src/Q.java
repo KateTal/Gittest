09b79961ada5c438bfc35fed11c365fc9fdfa8d0
Kate <Livia2008@yandex.ru>
2021-07-06 15:19:15 +0300

    added 1 file

---
+++ src/Q.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDate birthday = LocalDate.of(2021, 11, 30);
+        LocalDate nextBirthday = birthday.plusYears(1);
+        LocalDate firstBirthday = birthday.minusYears(35);
+        System.out.println(nextBirthday);
+        System.out.println(firstBirthday);
+    }
